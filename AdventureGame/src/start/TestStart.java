37971034cc0c5d3b452223755261de0ef5f452d7
Jaqobs <me@quan-sushi.com>
2014-12-08 10:58:00 +0100

    -added JFrame

--- AdventureGame/src/start/TestStart.java
+++ AdventureGame/src/start/TestStart.java
@@ -1,11 +1,14 @@
 package start;
 
+import GUI.MainFrame;
 import dungeon.Client;
 import dungeon.Factory;
 
 public class TestStart {
 	
 	public static void main(String[] args) {
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
 		Client c = new Client();
 		c.setFactory(new Factory());
 		c.makeRoom();
