40cba5a1039a44c14bd01893625a291efc4166b3
Jaqobs <me@quan-sushi.com>
2014-12-08 10:59:39 +0100

    -added JFrame and MenuItemFactory

---
+++ AdventureGame/src/GUI/MenuItemFactory.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.JMenuItem;
+
+public class MenuItemFactory {
+	
+	public static JMenuItem createMenuItem(String label, char mnemonic, String actionCommand) {
+		JMenuItem menuItem = new JMenuItem(label);
+		menuItem.setActionCommand(actionCommand);
+		menuItem.setMnemonic(mnemonic);
+		return menuItem;
+	}
+
+}
