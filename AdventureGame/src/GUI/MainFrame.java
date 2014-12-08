cc44569347e9a8aef469aacea1e44d0d4d2726bd
longdo93 <longdo93@gmx.de>
2014-12-08 15:59:02 +0100

    Update MainFrame.java

--- AdventureGame/src/GUI/MainFrame.java
+++ AdventureGame/src/GUI/MainFrame.java
@@ -7,33 +7,45 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
-import GUI.MenuItemFactory;
-
 public class MainFrame extends JFrame {
-	
+	buttonPanel buttonpanel = new buttonPanel();
+
 	public MainFrame() {
 		setTitle("Maze Game");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		/**
 		 * Sizing and centering app frame.
 		 */
-		setSize(800, 600);
+		int x = 0, y = 0, width = 800, height = 600;
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-		setLocation((d.width - getSize().width) / 2, (d.height - getSize().height) / 2);
-		
+		x = (int) ((d.getWidth() - width) / 2);
+		y = (int) ((d.getHeight() - height) / 2);
+		setBounds(x, y, width, height);
+
 		createMenuBar();
+		buttonpanel.createButtons();
+		getContentPane().add(buttonpanel.getbuttonPanel());
 	}
 
 	private void createMenuBar() {
 		JMenuBar menuBar = new JMenuBar();
+
 		JMenu menuFile = new JMenu("File");
+		JMenu menuFile2 = new JMenu("Figure");
+		JMenu menuFile3 = new JMenu("Help");
+		JMenu menuFile4 = new JMenu("Author");
 		menuFile.add(MenuItemFactory.createMenuItem("Open...", 'o', "file_open"));
-		menuFile.add(MenuItemFactory.createMenuItem("Save...",'s', "file_save"));
-		menuFile.add(MenuItemFactory.createMenuItem("Quit...",'q', "file_close"));
-		
+		menuFile.add(MenuItemFactory.createMenuItem("Save...", 's', "file_save"));
+		menuFile.add(MenuItemFactory.createMenuItem("Quit...", 'q', "file_close"));
+		menuFile2.add(MenuItemFactory.createMenuItem("Open...", 'o', "figure_open"));
+
 		menuBar.add(menuFile);
+		menuBar.add(menuFile2);
+		menuBar.add(menuFile3);
+		menuBar.add(menuFile4);
 		setJMenuBar(menuBar);
+
 	}
 
 }
