28efc1b4b0ae5278db9a2a82d6f542fd893e9924
longdo93 <longdo93@gmx.de>
2014-12-17 16:44:31 +0100

    rename buttons

--- AdventureGame/src/GUI/ButtonPanel.java
+++ AdventureGame/src/GUI/ButtonPanel.java
@@ -18,36 +18,15 @@ public class ButtonPanel {
 	private JPanel buttonPanel;
 	GameMaster gm=new GameMaster();
 	Player player=new Player();
-<<<<<<< HEAD
-	private JButton b1 = new JButton("North");
-	private JButton b2 = new JButton("West");
-	 private JButton b3 = new JButton("East");
-	private JButton b4 = new JButton("South");
-=======
 	private JButton north = new JButton("North");
 	private JButton west = new JButton("West");
 	 private JButton east = new JButton("East");
 	private JButton south = new JButton("South");
->>>>>>> master
 
 	public void createButtons() {
 
 		buttonPanel = new JPanel();
 		buttonPanel.setLayout(null);
-<<<<<<< HEAD
-		buttonPanel.add(b1);
-		buttonPanel.add(b2);
-		buttonPanel.add(b3);
-		buttonPanel.add(b4);
-		b1.setBounds(85, 435, 60, 30);
-		b2.setBounds(35, 465, 60, 30);
-		b3.setBounds(135, 465, 60, 30);
-		b4.setBounds(85, 495, 60, 30);
-	}
-	
-	public void createButtonListeners(){
-		this.b1.addActionListener(new ActionListener() {
-=======
 		buttonPanel.add(north);
 		buttonPanel.add(west);
 		buttonPanel.add(east);
@@ -60,7 +39,6 @@ public class ButtonPanel {
 	
 	public void createButtonListeners(){
 		this.north.addActionListener(new ActionListener() {
->>>>>>> master
 
 			public void actionPerformed(ActionEvent e) {
 				player.setDirection("N");
@@ -68,11 +46,7 @@ public class ButtonPanel {
 				}
 		});
 		
-<<<<<<< HEAD
-		this.b2.addActionListener(new ActionListener() {
-=======
 		this.west.addActionListener(new ActionListener() {
->>>>>>> master
 
 			public void actionPerformed(ActionEvent e) {
 				player.setDirection("W");
@@ -80,11 +54,7 @@ public class ButtonPanel {
 				}
 		});
 		
-<<<<<<< HEAD
-		this.b3.addActionListener(new ActionListener() {
-=======
 		this.east.addActionListener(new ActionListener() {
->>>>>>> master
 
 			public void actionPerformed(ActionEvent e) {
 				player.setDirection("E");
@@ -92,11 +62,7 @@ public class ButtonPanel {
 				}
 		});
 		
-<<<<<<< HEAD
-		this.b4.addActionListener(new ActionListener() {
-=======
 		this.south.addActionListener(new ActionListener() {
->>>>>>> master
 
 			public void actionPerformed(ActionEvent e) {
 				player.setDirection("S");
