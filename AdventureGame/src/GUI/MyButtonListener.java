86bbeb2e1a37f83de026899258ef5730bb166c21
longdo93 <longdo@192.168.0.10>
2015-01-05 23:01:28 +0100

    -new class mybuttonlistener

---
+++ AdventureGame/src/GUI/MyButtonListener.java
@@ -0,0 +1,34 @@
+package GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+
+import actors.GameMaster;
+import dungeon.Client;
+
+class MyButtonListener implements ActionListener {
+
+	private static MyButtonListener instance;
+
+	private MyButtonListener() {
+
+	}
+
+	public static MyButtonListener getInstance() {
+		if (instance == null) {
+			instance = new MyButtonListener();
+		}
+		return instance;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("open_map")) {
+			Client.getInstance().makeRoom();
+			Client.getInstance().startGame();
+		}
+
+	}
+
+}
